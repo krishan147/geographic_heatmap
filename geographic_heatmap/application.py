945167c2eb82ea48776aa110fad863df09b67c42
krishan vadher <krishan.vadher@gmail.com>
2019-04-15 11:39:26 +0100

    Added more data

--- geographic_heatmap/application.py
+++ geographic_heatmap/application.py
@@ -1,28 +1,23 @@
-def buildGeoheatmap(frame,chosen_kpi):
+import pandas as pd
+
+def buildGeoheatmap(frame):
     import folium
     import time
+    import json
     from PIL import Image
     import plotly.plotly as py
     import country_converter as coco
+    states = json.load(open('uk_cities.json'))
 
-    column_names = (list(frame.columns.values))
-
-
-
-    states = 'uk_cities.json'
-
-    m = folium.Map(location=[55.386670, -3.804810], tiles="Mapbox Bright", zoom_start=7)  # veritcal horizontal tiles = "Stamen Toner" Mapbox Bright
-    m.choropleth(geo_data=states, name='choropleth', data=frame, columns=['Region', 'Cost'],
+    m = folium.Map(location=[55.386670, -3.804810], tiles="Mapbox Bright", zoom_start=6)  # vertical horizontal tiles = "Stamen Toner" Mapbox Bright
+    m.choropleth(geo_data=states, name='choropleth', data=frame, columns=['City', 'Spend'],
                  key_on='properties.ctyua17nm', fill_color='Greens', fill_opacity=0.7, line_opacity=0.2,
                  legend_name='Spend')
     folium.LayerControl().add_to(m)
 
     m.save('buildGeoHeatmap.html')
 
-    # import selenium.webdriver
-    # driver = selenium.webdriver.PhantomJS()
-    # time.sleep(1)
-    # driver.set_window_size(1300, 1750)  # choose a resolution
-    # driver.get('buildGeoHeatmap.html')
-    # # You may need to add time.sleep(seconds) here
-    # driver.save_screenshot('buildGeoHeatmap.png')
+
+frame = pd.read_csv('sample_geographic_heatmap.csv')
+
+buildGeoheatmap(frame)
